84500d3b6e5781c236df4c7dea42b56825de416c
Hyun ju <judoree9629@gmail.com>
2025-03-31 15:30:40 +0900

    TeamCard

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,9 +3,11 @@ import { ProductCard } from '../../features/products/components/product-card';
 import { PostCard } from '../../features/community/components/post-card';
 import { IdeaCard } from '../../features/ideas/components/idea-card';
 import { Button } from '../components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../components/ui/card';
 import { Badge } from '../components/ui/badge';
 import { JobCard } from '~/features/jobs/components/job-card';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
+import { TeamCard } from '~/features/teams/components/team-card';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Home | Wemake' }, { name: 'description', content: 'Welcome to  We make' }];
@@ -101,6 +103,24 @@ export default function HomePage() {
               currency: '$',
             }}
             location="Korea Seoul"
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Find a team mate</h2>
+          <p className="text-xl font-light text-foreground">Join a team looking for a team member.</p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/teams">Explore all teams &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <TeamCard
+            id="teamId"
+            leaderUsername="judoree"
+            leaderAvatarUrl="https://github.com/judoree.png"
+            positions={['React Developer', 'Backend Developer', 'AI Developer']}
+            projectDescription="a new social media platform"
           />
         ))}
       </div>
